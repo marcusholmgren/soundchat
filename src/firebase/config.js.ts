56915495103f44720978c3751ca8868c24d0ce57
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-09-10 10:53:00 +0200

    chore: add firebase with default hosting configuration

---
+++ src/firebase/config.js.ts
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: '<your-api-key>',
+    authDomain: '<your-auth-domain>',
+    databaseURL: '<your-database-url>',
+    projectId: '<your-cloud-firestore-project>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-sender-id>',
+    appId: '<your-application-id>'
+};
+
+export default firebaseConfig;
