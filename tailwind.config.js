968f88fd5f01c3825154bb306cad8b76eafa8baa
Marcus Holmgren <marcus.holmgren1@gmail.com>
2021-02-21 20:32:21 +0100

    chore: upgrade firebase and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,6 @@ module.exports = {
   },
   variants: {},
   plugins: [
-    require('@tailwindcss/typography', '@tailwindcss/aspect-ratio', '@tailwindcss/forms'),
+    require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms'),
   ]
 };
