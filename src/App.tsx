18375b56a740ce955324f3db6c7752132014f2f8
Marcus Holmgren <marcus.holmgren1@gmail.com>
2021-04-14 21:59:18 +0200

    chore: upgrade dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,47 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 interface AppProps {}
 
 function App({}: AppProps) {
+    const navigate = useNavigate();
   return (
     <div className="App">
-      <div className="bg-yellow text-red-600 pt-6 text-left font-medium text-gray-700">
-        This is soundchat
-      </div>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <StartSection navigate={navigate} />
     </div>
   );
 }
+
+
+
+function StartSection({navigate}: { navigate: (str) => void}) {
+
+    function addClicked(e: any) {
+        e.preventDefault()
+        navigate("/add");
+    }
+  return (
+    <div className="bg-indigo-50">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-24 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 md:text-4xl">
+          <span className="block">Ready to dive in?</span>
+          <span className="block text-indigo-600">This is Soundchat</span>
+        </h2>
+        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+          <div className="inline-flex rounded-md shadow">
+            <a
+              href="/add"
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+              onClick={addClicked}
+            >
+              Add tune
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 
 export default App;
